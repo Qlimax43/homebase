28311bea5cb23eeb851f16a25ec73cad5667eea2
Lars van der Sangen <lvandersangen@senet.nl>
2015-05-06 09:56:48 +0200

    fix

--- index.php
+++ index.php
@@ -3,24 +3,24 @@
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css" >
 		<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" >
 
-		<script type="text/Javasctipt" src="assets/js/jquery-2.1.3.min.js">
-		<script type="text/Javasctipt" src="assets/js/npm.js">
-		<script type="text/Javasctipt" src="assets/js/bootstrap.min.js">
+        <script type="text/Javasctipt" src="assets/js/jquery-2.1.3.min.js"></script>
+		<script type="text/Javasctipt" src="assets/js/npm.js"></script>
+		<script type="text/Javasctipt" src="assets/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 	<div class="row">
 
-	<div class="col-xs-6 col-md-3 col-lg-3
-		<button class="btn btn-default">
+	<div class="col-xs-6 col-md-3 col-lg-3">
+        <button class="btn btn-default">knopje</button>
 	</div>
-	<div class="col-xs-6 col-md-3 col-lg-3
-		<button class="btn btn-warning">
+	<div class="col-xs-6 col-md-3 col-lg-3">
+        <button class="btn btn-warning">knopje</button>
 	</div>
-	<div class="col-xs-6 col-md-3 col-lg-3
-		<button class="btn btn-danger">
+	<div class="col-xs-6 col-md-3 col-lg-3">
+        <button class="btn btn-danger">knopje</button>
 	</div>
-	<div class="col-xs-6 col-md-3 col-lg-3
-		<button class="btn btn-primary">
+	<div class="col-xs-6 col-md-3 col-lg-3">
+        <button class="btn btn-primary">knopje</button>
 	</div>
 
 	</body>
