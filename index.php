f83bf358ce9be0d23b85d6fe29a0f5dc1b88c104
= <=>
2015-05-06 08:13:10 +0200

    push

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?= phpinfo();?>
+<html>
+	<head>
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+		<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" >
+
+		<script type="text/Javasctipt" src="assets/js/jquery-2.1.3.min.js">
+		<script type="text/Javasctipt" src="assets/js/npm.js">
+		<script type="text/Javasctipt" src="assets/js/bootstrap.min.js">
+	</head>
+	<body>
+	<div class="row">
+
+	<div class="col-xs-6 col-md-3 col-lg-3
+		<button class="btn btn-default">
+	</div>
+	<div class="col-xs-6 col-md-3 col-lg-3
+		<button class="btn btn-warning">
+	</div>
+	<div class="col-xs-6 col-md-3 col-lg-3
+		<button class="btn btn-danger">
+	</div>
+	<div class="col-xs-6 col-md-3 col-lg-3
+		<button class="btn btn-primary">
+	</div>
+
+	</body>
+</html>
